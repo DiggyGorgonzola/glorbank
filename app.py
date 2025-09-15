b0c7f5ed805be668f5e3a9bf547e3d43db8e49c9
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-09-15 15:24:03 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import request, render_template, redirect, url_for
+
+users_db = {}
+
+app = Flask(__name__)
+app.secret_key = "Glorbank"
+
+@app.route('/home.html')
+def home():
+  return render_template("home.html")
