096cc51c3f3db040818c0298fac85e68876cc2c5
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-09-16 15:18:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ users_db = {"Diggy Gorgonzola": 417}
 
 app = Flask(__name__)
 app.secret_key = "Glorbank"
-def check_password_hash(user_password_hash, password):
-  return users_db[username] == password
+
   
 @app.route('/', methods=["GET", "POST"])
 def starting():
@@ -27,7 +26,7 @@ def login():
       username = request.form['username']
       password = request.form['password']
       user_password_hash = users_db.get(username)
-      if user_password_hash and check_password_hash(user_password_hash, password):
+      if user_password_hash and users_db[username] == password:
         return render_template("indexi.html")
       return render_template("home.html", incorrect_password=True)
     else:
