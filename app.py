b150e8e080cfcd41b8fa7750ef2b4b8dbfb3cb6b
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-09-15 19:22:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ users_db = {}
 
 app = Flask(__name__)
 app.secret_key = "Glorbank"
-
-@app.route('/index.html')
+@app.route('/')
+def starting():
+  return render_remplate("home.html")
+@app.route('/home.html')
 def index():
   return render_template("home.html")
