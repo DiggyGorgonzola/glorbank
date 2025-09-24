534475af4172ffe7f1359f7d68776cf7985b4e46
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-09-24 09:51:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+#secret key!!!!
 app.secret_key = "Glorbank"
 
 
@@ -41,20 +42,30 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 session.begin()
+
+#Add a base admin account for testing purposes
 try:
   new_user = User(username="Diggy Gorgonzola", password="417", email=None, ip="127.0.0.1", admin=True)
   session.add(new_user)
   session.commit()
 except:
   session.rollback()
+
+#print the entire database for testing purposes
 a = session.query(User).all()
 for user in a:
   print([user.id, user.username, user.password, user.email, user.ip, user.admin])
+
+
 @app.route('/', methods=["GET", "POST"])
 def starting():
   if request.method == "POST":
+
+    #Save data entered from the form
     username = request.form['username']
     password = request.form['password']
+
+    #See if the credentials are valid. (WIP add IP 2fa)
     if not session.query(User).filter_by(username=username).all():
       return render_template("home.html", incorrect_password='true')
     elif session.query(User).filter_by(username=username).all()[0].password == password:
@@ -70,6 +81,8 @@ def indexi():
 @app.route('/register', methods=["GET", "POST"])
 def register():
   if request.method == "POST":
+    
+    #Save data entered from the form
     username = request.form['username']
     password = request.form['password']
     user_ip = request.remote_addr
@@ -77,7 +90,10 @@ def register():
     if request.form['email']:
       email = request.form['email']
 
+    #print data for testing purposes
     print((username, user_ip, password, email))
+
+    #Add the user to the database. Check if the username is already taken
     new_user = User(username=username, password=password, ip=user_ip, email=email, admin=False)
     if not session.query(User).filter_by(username=username).all():
       try:
