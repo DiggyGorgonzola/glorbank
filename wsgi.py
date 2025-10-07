7a79311341594e8af4a2e1c24ca182fa8b672872
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-10-07 10:21:25 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,10 @@ from app import app
 
 if __name__ == "__main__":
   app.run(debug=True)
+
+#I'm so skibidi I'm so skibidi skibidi skibidi skibidi biden. I'm so skibidi I'm so skibidi skibidi skibidi skibidi biden
+#If you're reading this, you've committed a felony, I think.
+#
+#Hello, world! I'm the most garbage programmer ever, but I'm the best Glorbenia's got lwk
+#-Diggy Gorgonzola, Oct 7th 2025
+#
