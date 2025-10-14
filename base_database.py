8727dabc35e7f972de81868e75d46bf0dc01503a
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-10-14 15:40:53 -0500

    Create base_database.py

---
+++ base_database.py
@@ -0,0 +1,21 @@
+# base_database.py
+
+# THIS DOESN"T WORK
+
+import datetime
+
+class BaseDatabase():
+    self.USERDATABASE = [
+  ["Diggy Gorgonzola", "417", None, "127.0.0.1", datetime.datetime.now(), 10, -1],
+  ["Dinky Gonky", "brd52009", None, "127.0.0.1", datetime.datetime.now(), 0, 1],
+  ["Dinky Gonky Alt", "brd52009", None, "127.0.0.1", datetime.datetime.now(), 0, 2],
+]
+    self.BANKDATABASE = BANKDATABASE = [
+  ["-999999", USERDATABASE[0][4], USERDATABASE[0][6]],
+  ["5", USERDATABASE[1][4], USERDATABASE[1][6]],
+  ["-99999", USERDATABASE[2][4], USERDATABASE[2][6]],
+]
+    def Apply():
+
+
+if __name__ == "__main__":
