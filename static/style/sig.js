5d33cb485bce268467c753c05915e9af473dc129
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-07 09:46:25 -0600

    Create sig.js

---
+++ static/style/sig.js
@@ -0,0 +1,17 @@
+/* sig.js */
+/* unused right now */
+
+/* 
+The parameters must always be:
+useracc = User type object
+bankacc = Bank type object
+adming = admin integer level
+*/
+
+function sig(user_id) {
+    let useracc={{ useracc | tojson}};
+    let bankacc={{ useracc | tojson}};
+    let adming={{adming}};
+    
+    var username=`${useracc[1]}`;
+};
