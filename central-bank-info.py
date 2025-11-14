019bef1a0d5c1a31ca7e8fdcef96b7e620450284
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-14 09:52:36 -0600

    Create central-bank-info.py

---
+++ central-bank-info.py
@@ -0,0 +1,14 @@
+#central-bank-info.py
+
+
+
+
+
+class BankInfo():
+  __init__(self):
+    self.suspicious_transaction_limit = 99
+    self.woolong_to_diamond = 5
+    self.woolong_to_parts = 0.05
+    self.woolong_to_usd = 0
+
+#I'm not sure if this will really be used. But it may be nice to have...
