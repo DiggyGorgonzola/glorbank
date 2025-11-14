d6c01cc80535ede5e09aaf19cb7300316a1b0f1d
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-14 13:56:28 -0600

    Update clock.js

--- static/style/clock.js
+++ static/style/clock.js
@@ -15,8 +15,19 @@ function updateClock() {
         });
 
     document.getElementById('clock-display').innerText = `${formattedDate} ${formattedTime}`;
+
 };
 function startClock() {
+    /*alternative to this line but idk if it'll work:
+    
+    let clockDisplay = document.createElement("div");
+    clockDisplay.id = "clock-display";
+    clockDisplay.style.position = "absolute";
+    clockDisplay.style.fontSize = "10px";
+    clockDisplay.style.textAlign = "left";
+    clockDisplay.style.textDecoration = "0px";
+    document.body.appendChild(clockDisplay);
+    */
     setInterval(updateClock, 1000);
 };
-
+startClock()
