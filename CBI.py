901b53677370ff3e8161d4ddf41208f436940afb
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-14 13:19:40 -0600

    Update CBI.py

--- CBI.py
+++ CBI.py
@@ -1,13 +1,16 @@
 #CBI.py
-suspicious_transaction_limit = 99
+suspicious_transaction_limit = 100
 woolong_to_diamond = 5
 woolong_to_parts = 0.05
 woolong_to_usd = 0
 LOCKDOWN = False
 SECRETKEY = "GINGINATOR3000"
+
+CHANGEVALUEALLOW = False
+# may not be needed depending on the hosting service.
 def changeValue(variable_id_change, variable_change, identification_code)
   current_time = datetime.datetime.now()
-  if identification_code != SECRETKEY and not LOCKDOWN:
+  if identification_code != SECRETKEY and not LOCKDOWN and CHANGEVALUEALLOW:
     LOCKDOWN = True
   else:
     if variable_id_change == 0:
