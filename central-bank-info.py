aa438d83cd8b765f382ecb58bf24a9b72a14be6d
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-14 10:20:00 -0600

    Update central-bank-info.py

--- central-bank-info.py
+++ central-bank-info.py
@@ -1,14 +1,7 @@
 #central-bank-info.py
-
-
-
-
-
-class BankInfo():
-  __init__(self):
-    self.suspicious_transaction_limit = 99
-    self.woolong_to_diamond = 5
-    self.woolong_to_parts = 0.05
-    self.woolong_to_usd = 0
+suspicious_transaction_limit = 99
+woolong_to_diamond = 5
+woolong_to_parts = 0.05
+woolong_to_usd = 0
 
 #I'm not sure if this will really be used. But it may be nice to have...
