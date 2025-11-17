7fa1f4502bb9268035306e3d3dd35a07527a3887
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-17 13:23:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 # app.py
 
 '''
-
-
-
-
-
 +-----------------------+
 | IMPORTS               |
 +-----------------------+
 '''
 from flask import Flask, render_template, request, redirect, url_for, jsonify, Blueprint
-from decimal import Decimal as decimal
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, text, Boolean, LargeBinary, JSON
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from flask_bcrypt import Bcrypt
+
+from sqlalchemy.orm import sessionmaker
+
 from werkzeug.security import generate_password_hash, check_password_hash
+
+from decimal import Decimal as decimal
+
+from appdata.database import Base, engine
+from appdata.models import User, Bank, Mail, Reports, OngoingTransaction, RegisteringOrganizations, Organization, Employees, Frozen
 from CBI import cbidict, HOMEREDIRECT
 
 import os, datetime, json
@@ -25,11 +24,6 @@ import os, datetime, json
 
 
 '''
-
-
-
-
-
 +-----------------------+
 | CONSTANTS             |
 +-----------------------+
@@ -56,131 +50,23 @@ BANKDATABASE = [
 
 
 '''
-
-
-
-
-
 +-----------------------+
 | DATABASE CREATION     |
 +-----------------------+
 '''
-# create the db link
 app = Flask(__name__)
-Base = declarative_base()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # SQLite file
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# create the db and encryption?
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
-#secret key!!!!
 app.secret_key = "Glorbank"
-# activate db
-engine = create_engine('sqlite:///users.db')
-Base.metadata.create_all(engine)
+Base.metadata.create_all(bind=engine)
 
 
 # create session <- move to wsgi.py please if possible thank you
 Session = sessionmaker(bind=engine)
 session = Session()
 session.begin()
-
-
-'''
-
-
-
-
-
-+-----------------------+
-| DATABASE TRAITS       |
-+-----------------------+
-'''
-class User(Base):
-  __tablename__ = "User"
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
-  password = db.Column(db.String(80), nullable=False)
-  email = db.Column(db.String(80), nullable=True)
-  admin = db.Column(db.Integer)
-  ip = db.Column(db.Text)
-  accdate = db.Column(db.DateTime)
-  national_id = db.Column(db.Integer, nullable=False, unique=True)
-
-class Bank(Base):
-  __tablename__ = "Bank"
-  id = db.Column(db.Integer, primary_key=True)
-  woolong = db.Column(db.String, nullable=False)
-  parts = db.Column(db.String, nullable=False)
-  credit = db.Column(db.String, nullable=False)
-  accdate = db.Column(db.DateTime)
-  national_id = db.Column(db.Integer, nullable=False, unique=True)
-
-class Mail(Base):
-  __tablename__ = "Mail"
-  id = db.Column(db.Integer, primary_key=True)
-  acc_id_to = db.Column(db.Integer, nullable=False)
-  title = db.Column(db.String, nullable=False)
-  message = db.Column(db.String, nullable=False)
-  contact = db.Column(db.String)
-
-class OngoingTransactions(Base):
-  __tablename__ = "OngoingTransactions"
-  id = db.Column(db.Integer, primary_key=True)
-  woolong = db.Column(db.String, nullable=False)
-  information = db.Column(db.String, nullable=True)
-  date = db.Column(db.DateTime)
-  natid_from = db.Column(db.Integer, nullable=False, unique=True)
-  natid_to = db.Column(db.Integer, nullable=False, unique=True)
-
-# Organizations must go through a verification process before they are added to the actual database.
-class RegisteringOrganizations(Base):
-  __tablename__ = "RegisteringOrganizations"
-  id = db.Column(db.Integer, primary_key=True)
-  accdate = db.Column(db.DateTime)
-  name = db.Column(db.String, nullable=False)
-  email = db.Column(db.String, nullable=False)
-  phone = db.Column(db.String, nullable=False)
-  orgpass = db.Column(db.String, nullable=False)
-
-
-class Organization(Base):
-  __tablename__ = "Organization"
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(80), unique=True, nullable=False)
-  email = db.Column(db.String(80), nullable=True)
-  accdate = db.Column(db.DateTime)
-  orgid = db.Column(db.String, unique=True, nullable=False)
-
-# Perhaps make a separate table for each organization. Procedurally.
-class Employees(Base):
-  __tablename__ = "Employees"
-  id = db.Column(db.Integer, primary_key=True)
-  organization = db.Column(db.String, nullable=False)
-  primary_owner = db.Column(db.String, nullable=True)
-  secondary_owners = db.Column(db.String, nullable=True)
-  org_admins = db.Column(db.String, nullable=True)
-  org_employees = db.Column(db.String, nullable=True)
-
-
-# For frozen users
-class Frozen(Base):
-  __tablename__ = "Frozen"
-  id = db.Column(db.Integer, primary_key=True)
-  national_id = db.Column(db.Integer, nullable=False, unique=True)
-
-class Reports(Base):
-  __tablename__ = "Reports"
-  id = db.Column(db.Integer, primary_key=True)
-  woolong = db.Column(db.String, nullable=False)
-  information = db.Column(db.String, nullable=True)
-  date = db.Column(db.DateTime)
-  id_from = db.Column(db.String, nullable=False)
-  id_to = db.Column(db.String, nullable=False)
-  # fix whatever's wrong with this!
-
 '''
 
 
