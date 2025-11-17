74f3b98f4b072de43664769dadb6f9e29cef6a78
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-17 11:24:48 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,8 @@ from app import app, Delete
 if __name__ == "__main__":
   #Delete()
   #DELETES ENTIRE DATABASE
-  debug = app.DEBUG[0] if app.DEBUG is not None else False
-  app.run(debug=debug)
+  debug = app.cbidict["debug"][0] if app.cbidict is not None else False
+  app.run(debug=debug) #<- This might not work. If it doesn't, use app.run(debug=True) or smth...
 
 #I'm so skibidi I'm so skibidi skibidi skibidi skibidi biden. I'm so skibidi I'm so skibidi skibidi skibidi skibidi biden
 #If you're reading this, you've committed a felony, I think.
@@ -12,3 +12,4 @@ if __name__ == "__main__":
 #Hello, world! I'm the most garbage programmer ever, but I'm the best Glorbenia's got lwk
 #-Diggy Gorgonzola, Oct 7th 2025
 #
+# Wow this was a long time ago... It's November 17th 2025 now.
