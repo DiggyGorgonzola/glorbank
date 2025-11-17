3195902aa512df782b66dab21b67d7a5e2307bb7
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-17 09:55:22 -0600

    Update CBI.py

--- CBI.py
+++ CBI.py
@@ -1,4 +1,7 @@
 #CBI.py
+
+# MAIN BANK VARIABLES
+# ---------
 suspicious_transaction_limit = 100
 woolong_to_diamond = 5
 woolong_to_parts = 0.05
@@ -7,9 +10,14 @@ woolong_value_in_reserves = 0
 LOCKDOWN = False
 SECRETKEY = "GINGINATOR3000"
 HISTORY = []
+# ---------
 
-CHANGEVALUEALLOW = False
+
+
+# CHANGE BANK VALUES
+# ---------
 # may not be needed depending on the hosting service.
+CHANGEVALUEALLOW = False
 def changeValue(variable_id_change, variable_change, identification_code):
   current_time = datetime.datetime.now()
   if identification_code != SECRETKEY and not LOCKDOWN:
@@ -26,7 +34,7 @@ def changeValue(variable_id_change, variable_change, identification_code):
     elif variable_id_change == 4:
       woolong_value_in_reserves = variable_change
     HISTORY.append(f"{current_time}|variable id change: {variable_id_change}|variable change {variable_change}")
-  
+# ---------
 
 #I'm not sure if this will really be used. But it may be nice to have...
 # It will be used to hold all the bank info that we dont want anybody to be able to change. If someone does change it, it may spell disaster!
