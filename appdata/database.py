3e72d55860fcfb6c5f018b95763c75d6b258a82d
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-17 15:42:16 -0600

    Update database.py

--- appdata/database.py
+++ appdata/database.py
@@ -1,17 +1,21 @@
 # database.py
+from CBI import debug
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///users.db"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+def start_session():
+    from sqlalchemy.orm import sessionmaker
+    session = sessionmaker(bind=engine)()
+    session.begin()
+    return session
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def Print(*args, documentation=False):
+    if debug[0]:
+        print(" ".join(args))
+    
