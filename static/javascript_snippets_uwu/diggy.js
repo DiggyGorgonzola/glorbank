b9e99ffb5a574f2a60e769585bb7a6112997a4be
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-18 09:55:44 -0600

    Create diggy.js

---
+++ static/javascript_snippets_uwu/diggy.js
@@ -0,0 +1,13 @@
+//diggy.js
+
+
+//maybe useful functions
+
+
+function setText(elem, text) {
+    document.getElementById("elem").innerText = text;
+};
+
+function byId(elem) {
+    return document.getElementById(elem);
+};
