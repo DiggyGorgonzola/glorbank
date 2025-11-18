56e45191f0ac5b0169007273d1c455cc0d3331af
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-17 18:31:57 -0600

    Update sig.js

--- static/style/sig.js
+++ static/style/sig.js
@@ -1,17 +1,12 @@
-/* sig.js */
-/* unused right now */
-
-/* 
-The parameters must always be:
-useracc = User type object
-bankacc = Bank type object
-adming = admin integer level
-*/
-
-function sig(user_id) {
-    let useracc={{ useracc | tojson}};
-    let bankacc={{ useracc | tojson}};
-    let adming={{adming}};
-    
-    var username=`${useracc[1]}`;
+function Signature(signature) {
+    fetch('/getacc', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify(signature),})
+    .then(response => response.json())
+    .then(data => {
+        console.log('POST response:', data);
+        account = data.response
+    })
+    return account;
 };
