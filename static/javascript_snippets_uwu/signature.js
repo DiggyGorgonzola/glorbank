4785ec24b0823782a09133bc92ed572a3639a6e0
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-11-20 09:54:29 -0600

    Update signature.js

--- static/javascript_snippets_uwu/signature.js
+++ static/javascript_snippets_uwu/signature.js
@@ -1,15 +1,23 @@
 // signature.js
 
 // for the new signature system
+async function fetchSignature(signature) {
+    try {
+        const response = await fetch('/getacc', {method: "POST", headers: {'Content-Type': 'application/json'}, body:JSON.stringify(signature)});
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let data = await response.json();
+        console.log(data);
+        return data; // The data is returned here
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+};
 function Signature(signature) {
-    fetch('/getacc', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json',},
-        body: JSON.stringify(signature),})
-    .then(response => response.json())
-    .then(data => {
-        console.log('POST response:', data);
-        account = data.response
-    })
-    return account;
+    fetchSignature(signature).then(result => {
+        if (result) {
+            return Array.from(result.response);
+        };
+    });
 };
